e39b6705cbed870511739d8d43cbe640943a7325
Subhannita Sarcar <35349991+subhannita1994@users.noreply.github.com>
2020-04-08 02:10:42 -0400

    Developer options (#2)
    * classpaths updated
    
    * feat: compatibility with mac os
    
    * merged working feature
    
    * added developer options
    
    fix: mac os processBuilder error: printing output from static file
    fix: console color of : red for ’ error’ instead of ‘error’, pink for ‘warning’
    Feat: added onAction from buttons typical and expert to the corresponding methods in InitController
    Feat: added options under sample.fxml:Developer options
    Feat: added func for dev options in Controller, modified processBuilder command to accommodate dev options
    
    * Add files via upload
    
    just the raw indenter
    
    * Add files via upload
    
    load a file(auto indented)
    
    * scrapped "add developer option"
    
    * added optimizer class to package sample
    
    Co-authored-by: Hesamedin <47093517+hsmsharingan@users.noreply.github.com>

--- Code Window/out.cpp
+++ Code Window/out.cpp
@@ -1,1 +1,14 @@
-jsbvbsvosnvoindso
+#include <iostream> 
+  
+class Test { 
+private: 
+	Test(){}
+    ~Test() {} 
+}; 
+int main() 
+{ 
+    Test* t;
+    int a$var = 20;
+    std::cout<<a$var;
+    return 0;
+}
